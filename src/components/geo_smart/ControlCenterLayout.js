63934de9c4913b71b9f3bfd1f099b6edaabbde8b
Felipe Barraza <felipe.barraza.vega@gmail.com>
2026-06-02 18:48:32 -0400

    feat(control-center): migrate KPI Cards to SmartKPICard with SmartHydro gradients

--- src/components/geo_smart/ControlCenterLayout.js
+++ src/components/geo_smart/ControlCenterLayout.js
@@ -6,9 +6,9 @@ import {
   FaClipboardCheck,
   FaExclamationTriangle,
 } from "react-icons/fa";
-import KPICard from "./KPICard";
+import { SmartKPICard } from "../../shared/ui";
+import { smarthydro } from "../../theme/smarthydro.tokens";
 import ControlCenterChat from "./ControlCenterChat";
-import { ikoluTokens, kpiGradients } from "../../theme";
 
 const { Text } = Typography;
 const { useToken } = theme;
@@ -31,36 +31,36 @@ const ControlCenterLayout = memo(({
       {/* KPIs */}
       <Row id="cc-kpi-cards" gutter={[12, 12]} style={{ marginBottom: 8 }}>
         <Col xs={12} sm={6} md={6}>
-          <KPICard
-            icon={<FaMapMarkerAlt style={{ fontSize: 20, color: ikoluTokens.colorWhite }} />}
+          <SmartKPICard
+            icon={<FaMapMarkerAlt style={{ fontSize: 18, color: "#fff" }} />}
             label="Total Puntos"
             value={overview.total_points || 0}
-            gradient={kpiGradients.primary}
+            gradient={smarthydro.gradients.primary}
           />
         </Col>
         <Col xs={12} sm={6} md={6}>
-          <KPICard
-            icon={<FaBroadcastTower style={{ fontSize: 20, color: ikoluTokens.colorWhite }} />}
+          <SmartKPICard
+            icon={<FaBroadcastTower style={{ fontSize: 18, color: "#fff" }} />}
             label="Telemetría Activa"
             value={`${overview.points_with_telemetry || 0}`}
             suffix={`/${overview.total_points || 0}`}
-            gradient={kpiGradients.secondary}
+            gradient={smarthydro.gradients.info}
           />
         </Col>
         <Col xs={12} sm={6} md={6}>
-          <KPICard
-            icon={<FaClipboardCheck style={{ fontSize: 20, color: ikoluTokens.colorWhite }} />}
+          <SmartKPICard
+            icon={<FaClipboardCheck style={{ fontSize: 18, color: "#fff" }} />}
             label="Cumplimiento Normativo"
             value={overview.points_with_compliance || 0}
-            gradient={kpiGradients.secondary}
+            gradient={smarthydro.gradients.success}
           />
         </Col>
         <Col xs={12} sm={6} md={6}>
-          <KPICard
-            icon={<FaExclamationTriangle style={{ fontSize: 20, color: ikoluTokens.colorWhite }} />}
+          <SmartKPICard
+            icon={<FaExclamationTriangle style={{ fontSize: 18, color: "#fff" }} />}
             label="Warnings"
             value={warningsList.length}
-            gradient={kpiGradients.secondary}
+            gradient={smarthydro.gradients.accent}
             onClick={
               warningsList.length > 0
                 ? () => {
